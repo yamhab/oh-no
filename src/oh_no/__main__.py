0b0e6dbdf6874b032bbed3ecc4a011a3c7629b22
Yamin Shihab <yaminshihab123@gmail.com>
2026-03-27 19:34:52 -0600

    fix: document all code and implement encapsulation by making most things private

--- src/oh_no/__main__.py
+++ src/oh_no/__main__.py
@@ -1,3 +1,5 @@
+"""UNO in the terminal."""
+
 import contextlib
 import dataclasses
 import enum
@@ -6,23 +8,25 @@ import random
 import blessed
 import wcwidth
 
-MIN_WIDTH = 80
-MIN_HEIGHT = 24
+_MIN_WIDTH = 80
+_MIN_HEIGHT = 24
 
-START_LOGO = """  ___   __        ____  _____         _
+_START_LOGO = """  ___   __        ____  _____         _
  .'   `.[  |      |_   \\|_   _|       | |
 /  .-.  \\| |--.     |   \\ | |   .--.  | |
 | |   | || .-. |    | |\\ \\| | / .'`\\ \\| |
 \\  `-'  /| | | |   _| |_\\   |_| \\__. ||_|
  `.___.'[___]|__] |_____|\\____|'.__.' (_)"""
 
-MIN_PLAYERS = 2
-MAX_PLAYERS = 8
+_MIN_PLAYERS = 2
+_MAX_PLAYERS = 8
 
-HAND_CARDS = 7
+_HAND_CARDS = 7
 
 
-class Color(enum.IntEnum):
+class _Color(enum.IntEnum):
+    """A card's color."""
+
     RED = enum.auto()
     YELLOW = enum.auto()
     GREEN = enum.auto()
@@ -30,6 +34,7 @@ class Color(enum.IntEnum):
     WILD = enum.auto()
 
     def render(self, term: blessed.Terminal) -> str:
+        """Return the colored string to be printed to the terminal of the card's color."""
         rendered = ""
         match self:
             case self.RED:
@@ -45,7 +50,9 @@ class Color(enum.IntEnum):
         return rendered
 
 
-class Type(enum.IntEnum):
+class _Type(enum.IntEnum):
+    """A card's type."""
+
     NUMBER = enum.auto()
     SKIP = enum.auto()
     REVERSE = enum.auto()
@@ -54,6 +61,7 @@ class Type(enum.IntEnum):
     WILD_DRAW_FOUR = enum.auto()
 
     def render(self) -> str:
+        """Return the colored string to be printed to the terminal of the card's type."""
         rendered = ""
         match self:
             case self.SKIP:
@@ -68,22 +76,26 @@ class Type(enum.IntEnum):
 
 
 @dataclasses.dataclass(order=True)
-class Card:
-    color: Color
-    type: Type
+class _Card:
+    """An Oh No card."""
+
+    color: _Color
+    type: _Type
     number: int | None = None
 
     def render(self, term: blessed.Terminal) -> str:
-        if self.type == Type.NUMBER:
+        """Return the colored string to be printed to the terminal of the card."""
+        if self.type == _Type.NUMBER:
             return self.color.render(term) + " " + str(self.number) + term.normal
-        if self.type == Type.WILD:
+        if self.type == _Type.WILD:
             return self.color.render(term) + term.normal
         return self.color.render(term) + " " + self.type.render() + term.normal
 
-    def playable(self, last: Card) -> bool:
+    def playable(self, last: _Card) -> bool:
+        """Return whether its possible to play a card after another."""
         return (
-            self.type in {Type.WILD, Type.WILD_DRAW_FOUR}
-            or self.type == last.type != Type.NUMBER
+            self.type in {_Type.WILD, _Type.WILD_DRAW_FOUR}
+            or self.type == last.type != _Type.NUMBER
             or self.color == last.color
             or self.number == last.number is not None
         )
@@ -91,61 +103,66 @@ class Card:
 
 @dataclasses.dataclass
 class Game:
+    """All the game's state."""
+
     term: blessed.Terminal
-    num: int = 0
-    current: int = 0
-    direction: int = 1
-    deck: list[Card] = dataclasses.field(default_factory=list)
-    hands: list[list[Card]] = dataclasses.field(default_factory=list)
-    stack: list[Card] = dataclasses.field(default_factory=list)
-    skip: bool = False
-    draw: int = 0
+    _num: int = 0
+    _current: int = 0
+    _direction: int = 1
+    _deck: list[_Card] = dataclasses.field(default_factory=list)
+    _hands: list[list[_Card]] = dataclasses.field(default_factory=list)
+    _stack: list[_Card] = dataclasses.field(default_factory=list)
+    _skip: bool = False
+    _draw: int = 0
 
     def play(self) -> None:
-        if self.term.width < MIN_WIDTH or self.term.height < MIN_HEIGHT:
+        """Run the game's main loop."""
+        if self.term.width < _MIN_WIDTH or self.term.height < _MIN_HEIGHT:
             print(
-                f"Minimum terminal dimensions: {MIN_WIDTH}x{MIN_HEIGHT}\nCurrent terminal \
+                f"Minimum terminal dimensions: {_MIN_WIDTH}x{_MIN_HEIGHT}\nCurrent terminal \
 dimensions: {self.term.width}x{self.term.height}",
             )
             return
 
         with self.term.fullscreen():
-            self.setup_players()
-            self.setup_cards()
+            self._setup_players()
+            self._setup_cards()
 
-            for _ in range(self.num):
+            for _ in range(self._num):
                 print(self.term.clear() + self.term.center("~~~ PLAYER 1's TURN ~~~"))
-                self.print_hand()
-                self.choose_card()
-                self.card_action()
+                self._print_hand()
+                self._choose_card()
+                self._card_action()
                 with self.term.cbreak(), self.term.hidden_cursor():
                     print("Press any key to continue... ")
                     self.term.inkey()
 
-                while len(self.hands[self.current]) != 0:
-                    self.turn()
+                while len(self._hands[self._current]) != 0:
+                    self._turn()
                     with self.term.cbreak(), self.term.hidden_cursor():
                         print("Press any key to continue... ")
                         self.term.inkey()
 
-                print(f"{self.term.clear()}Player {self.current + 1} wins!")
+                print(f"{self.term.clear()}Player {self._current + 1} wins!")
 
-    def setup_players(self) -> None:
+    def _setup_players(self) -> None:
+        """Greet the player and get the number of players."""
         print(self.term.clear() + "\n")
-        for line in START_LOGO.splitlines():
+        for line in _START_LOGO.splitlines():
             print(self.term.center(line))
         print("\n" + self.term.center("Press CTRL-C to exit at any time"))
         print(self.term.center("Welcome to Oh No! How many people will be playing (2-8)?"))
 
-        while self.num < MIN_PLAYERS or self.num > MAX_PLAYERS:
+        while self._num < _MIN_PLAYERS or self._num > _MAX_PLAYERS:
             with contextlib.suppress(ValueError):
-                self.num = int(self.center_input("> "))
+                self._num = int(self._center_input("> "))
             print(
                 self.term.move_x(0) + self.term.move_up() + self.term.clear_eol(),
                 end="",
             )
 
-    def center_input(self, prompt: str) -> str:
+    def _center_input(self, prompt: str) -> str:
+        """Read a string from standard input with a centered prompt."""
         if prompt.isascii() and prompt.isprintable():
             text_width = len(prompt)
         else:
@@ -154,29 +171,31 @@ dimensions: {self.term.width}x{self.term.height}",
         left_pad = total_padding // 2 + (total_padding & self.term.width & 1) - 1
         return input(" " * left_pad + prompt)
 
-    def setup_cards(self) -> None:
-        for color in [Color.RED, Color.YELLOW, Color.GREEN, Color.BLUE]:
-            self.deck.append(Card(color, Type.NUMBER, 0))
+    def _setup_cards(self) -> None:
+        """Shuffle a deck and deal an appropriate number of cards to each player."""
+        for color in [_Color.RED, _Color.YELLOW, _Color.GREEN, _Color.BLUE]:
+            self._deck.append(_Card(color, _Type.NUMBER, 0))
             for number in range(1, 10):
-                self.deck.extend(Card(color, Type.NUMBER, number) for _ in range(2))
-            self.deck.extend(Card(color, Type.SKIP) for _ in range(2))
-            self.deck.extend(Card(color, Type.REVERSE) for _ in range(2))
-            self.deck.extend(Card(color, Type.DRAW_TWO) for _ in range(2))
-        self.deck.extend(Card(Color.WILD, Type.WILD) for _ in range(4))
-        self.deck.extend(Card(Color.WILD, Type.WILD_DRAW_FOUR) for _ in range(4))
-        random.shuffle(self.deck)
+                self._deck.extend(_Card(color, _Type.NUMBER, number) for _ in range(2))
+            self._deck.extend(_Card(color, _Type.SKIP) for _ in range(2))
+            self._deck.extend(_Card(color, _Type.REVERSE) for _ in range(2))
+            self._deck.extend(_Card(color, _Type.DRAW_TWO) for _ in range(2))
+        self._deck.extend(_Card(_Color.WILD, _Type.WILD) for _ in range(4))
+        self._deck.extend(_Card(_Color.WILD, _Type.WILD_DRAW_FOUR) for _ in range(4))
+        random.shuffle(self._deck)
 
-        for hand in range(self.num):
-            self.hands.append(self.deck[-HAND_CARDS:])
-            del self.deck[-HAND_CARDS:]
-            self.hands[hand].sort()
+        for hand in range(self._num):
+            self._hands.append(self._deck[-_HAND_CARDS:])
+            del self._deck[-_HAND_CARDS:]
+            self._hands[hand].sort()
 
-    def print_hand(self, playable: list[int] | None = None) -> None:
+    def _print_hand(self, playable: list[int] | None = None) -> None:
+        """Print the current player's hand and list cards playable on the stack's last card."""
         print("Here are the cards in your hand:")
-        for i, card in enumerate(self.hands[self.current]):
+        for i, card in enumerate(self._hands[self._current]):
             print(f"{i + 1}: {card.render(self.term)}")
-        if len(self.stack) != 0:
-            print(f"The last played card on the stack was a {self.stack[-1].render(self.term)}.")
+        if len(self._stack) != 0:
+            print(f"The last played card on the stack was a {self._stack[-1].render(self.term)}.")
         if playable:
             print("You may play one of the following cards from your hand:")
             for i, card in enumerate(playable):
@@ -185,13 +204,14 @@ dimensions: {self.term.width}x{self.term.height}",
                 else:
                     print(card + 1)
 
-    def choose_card(self, playable: list[int] | None = None) -> None:
+    def _choose_card(self, playable: list[int] | None = None) -> None:
+        """Ask the current player for what card from their hand to play, and play it if possible."""
         while True:
             with contextlib.suppress(ValueError, IndexError):
                 choice = int(input("Which card would you like to play? ")) - 1
                 if not playable or choice in playable:
-                    self.play_card(choice)
-                    if len(self.hands[self.current]) == 1:
+                    self._play_card(choice)
+                    if len(self._hands[self._current]) == 1:
                         print("Oh No!")
                     break
             print(
@@ -199,42 +219,45 @@ dimensions: {self.term.width}x{self.term.height}",
                 end="",
             )
 
-    def play_card(self, choice: int) -> None:
-        self.stack.append(self.hands[self.current][choice])
-        print("You play a " + self.hands[self.current][choice].render(self.term))
-        del self.hands[self.current][choice]
+    def _play_card(self, choice: int) -> None:
+        """Play the card indexed from the current player's hand."""
+        self._stack.append(self._hands[self._current][choice])
+        print("You play a " + self._hands[self._current][choice].render(self.term))
+        del self._hands[self._current][choice]
 
-    def card_action(self) -> None:
-        last = self.stack[-1]
+    def _card_action(self) -> None:
+        """Perform the stack's last played card's action (based off of its type), if needed."""
+        last = self._stack[-1]
         match last.type:
-            case Type.SKIP:
-                self.skip = True
-            case Type.REVERSE:
-                self.direction *= -1
-            case Type.DRAW_TWO:
-                self.skip = True
-                self.draw = 2
-            case Type.WILD:
-                self.choose_color()
-            case Type.WILD_DRAW_FOUR:
-                self.choose_color()
-                self.skip = True
-                self.draw = 4
+            case _Type.SKIP:
+                self._skip = True
+            case _Type.REVERSE:
+                self._direction *= -1
+            case _Type.DRAW_TWO:
+                self._skip = True
+                self._draw = 2
+            case _Type.WILD:
+                self._choose_color()
+            case _Type.WILD_DRAW_FOUR:
+                self._choose_color()
+                self._skip = True
+                self._draw = 4
 
-    def choose_color(self) -> None:
+    def _choose_color(self) -> None:
+        """Ask the current player for what color they'd like to switch their wild card to."""
         while True:
             match input("Which color would you like to switch to (R, Y, G, B)? ").lower():
                 case "r":
-                    self.stack[-1].color = Color.RED
+                    self._stack[-1].color = _Color.RED
                     break
                 case "y":
-                    self.stack[-1].color = Color.YELLOW
+                    self._stack[-1].color = _Color.YELLOW
                     break
                 case "g":
-                    self.stack[-1].color = Color.GREEN
+                    self._stack[-1].color = _Color.GREEN
                     break
                 case "b":
-                    self.stack[-1].color = Color.BLUE
+                    self._stack[-1].color = _Color.BLUE
                     break
                 case _:
                     print(
@@ -242,67 +265,72 @@ dimensions: {self.term.width}x{self.term.height}",
                         end="",
                     )
 
-    def turn(self) -> None:
-        self.rotate()
-        print(self.term.clear() + self.term.center(f"~~~ PLAYER {self.current + 1}'s TURN ~~~"))
+    def _turn(self) -> None:
+        """Go through the current player's turn."""
+        self._rotate()
+        print(self.term.clear() + self.term.center(f"~~~ PLAYER {self._current + 1}'s TURN ~~~"))
 
-        for _ in range(self.draw):
-            self.draw_card()
-        self.draw = 0
+        for _ in range(self._draw):
+            self._draw_card()
+        self._draw = 0
 
-        if self.skip:
-            self.print_hand()
+        if self._skip:
+            self._print_hand()
             print("Turn skipped")
-            self.skip = False
+            self._skip = False
             return
 
-        playable = self.playable_cards()
+        playable = self._playable_cards()
         card_played = False
         if len(playable) == 0:
             print("You have no valid cards to play")
-            self.draw_card()
-            playable = self.playable_cards()
+            self._draw_card()
+            playable = self._playable_cards()
             if len(playable) == 1:
-                self.print_hand(playable)
-                self.choose_card(playable)
+                self._print_hand(playable)
+                self._choose_card(playable)
                 card_played = True
             else:
-                self.print_hand()
+                self._print_hand()
         else:
-            self.print_hand(playable)
-            self.choose_card(playable)
+            self._print_hand(playable)
+            self._choose_card(playable)
             card_played = True
 
         if card_played:
-            self.card_action()
+            self._card_action()
 
-    def rotate(self) -> None:
-        self.current += self.direction
-        if self.current < 0:
-            self.current += self.num
+    def _rotate(self) -> None:
+        """Switch to the next player based off of the direction and number of players."""
+        self._current += self._direction
+        if self._current < 0:
+            self._current += self._num
         else:
-            self.current %= self.num
+            self._current %= self._num
 
-    def draw_card(self) -> None:
-        if len(self.deck) == 0:
-            self.deck = self.stack.copy()
-            self.stack.clear()
-            self.deck.sort()
+    def _draw_card(self) -> None:
+        """Shuffle the stack into the deck if needed, and draw a card for the current player."""
+        if len(self._deck) == 0:
+            self._deck = self._stack.copy()
+            self._stack.clear()
+            self._deck.sort()
 
-        card = self.deck.pop()
+        card = self._deck.pop()
         print(f"You draw a {card.render(self.term)} from the deck")
-        self.hands[self.current].append(card)
-        self.hands[self.current].sort()
+        self._hands[self._current].append(card)
+        self._hands[self._current].sort()
 
-    def playable_cards(self) -> list[int]:
+    def _playable_cards(self) -> list[int]:
+        """Return a list of indices to all the playable cards from the current player's hand."""
         playable = []
-        for i, card in enumerate(self.hands[self.current]):
-            if card.playable(self.stack[-1]):
+        for i, card in enumerate(self._hands[self._current]):
+            if card.playable(self._stack[-1]):
                 playable.append(i)
         return playable
 
 
 def main() -> None:
+    """Entry point of the game."""
     term = blessed.Terminal()
     game = Game(term)
     game.play()
